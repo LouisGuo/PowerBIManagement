6b41d4166b5e092992b6671c27fc486127bb9093
LouisGuo <2213429531@qq.com>
2016-09-16 19:56:26 +0800

    Embedded  Dll

--- VCloud.PowerBIManager/VCloud.PowerBIManager/Program.cs
+++ VCloud.PowerBIManager/VCloud.PowerBIManager/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,6 +10,31 @@ namespace VCloud.PowerBIManager
 {
     static class Program
     {
+        static Program()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+                var thisAssembly = Assembly.GetExecutingAssembly();
+                FileHelper.AppendLog("Request Assembly---------------------------- " + args.Name);
+                var name = args.Name.Substring(0, args.Name.IndexOf(',')) + ".dll";
+                var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(name));
+                if (resources.Count() > 0)
+                {
+                    var resourceName = resources.First();
+                    using (var stream = thisAssembly.GetManifestResourceStream(resourceName))
+                    {
+                        if (stream == null) return null;
+                        var block = new byte[stream.Length];
+                        stream.Read(block, 0, block.Length);
+                        var result = Assembly.Load(block);
+                        FileHelper.AppendLog("Load Assembly successfully---------------------------- " + result.FullName);
+                        return result;
+                    }
+                }
+                return null;
+            };
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
